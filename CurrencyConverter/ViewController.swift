3192847f38da413b8bc9b31912eaca21a17d4ec8
thadeAUx <tja0010@auburn.edu>
2025-02-19 19:46:04 -0600

    Added IBAction for switches, textfield, and button

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var euroConv : Float = 0.96
+    var jpyConv : Float = 150.75
+    var gbpConv : Float = 0.79
+    var audConv : Float = 1.58
+    
+    @IBOutlet weak var currencyInput: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func eurSwitch(_ sender: UISwitch) {
+        
+        
+        
+    }
+    
+    @IBAction func jpySwitch(_ sender: UISwitch) {
+    }
+    
+    @IBAction func gbpSwitch(_ sender: UISwitch) {
+    }
+    
+    @IBAction func audSwitch(_ sender: UISwitch) {
+    }
+    
+    @IBAction func convertButton(_ sender: UIButton) {
+    }
+    
 }
 
